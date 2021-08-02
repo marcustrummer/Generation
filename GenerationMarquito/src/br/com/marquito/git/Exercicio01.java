b3661a580c57f05ba72a0a7d0eb697f680e403c9
marcustrummer <72166193+marcustrummer@users.noreply.github.com>
2021-08-02 20:17:22 -0300

    Adicionando arquivos via upload

---
+++ GenerationMarquito/src/br/com/marquito/git/Exercicio01.java
@@ -0,0 +1,27 @@
+package br.com.marquito.git;
+
+import java.util.Scanner;
+
+/*
+ * 1. Faça um sistema que leia a idade de uma pessoa expressa em anos, meses e
+dias e mostre-a expressa apenas em dias.*/
+public class Exercicio01 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int ano, mes, dia;
+		
+		double resultado;
+		
+		System.out.println("\"Digite a sua idade em anos, meses e dias respectivamente\n");
+		ano = entrada.nextInt();
+		mes = entrada.nextInt();
+		dia = entrada.nextInt();
+		
+		resultado = ((ano*365) + (mes*30) + dia);
+		
+		System.out.println("Resultado: " + resultado);
+		entrada.close();
+	}
+
+}
